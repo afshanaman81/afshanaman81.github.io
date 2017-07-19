c8a8519669eafffefded1821afa42b65beea252f
afshanaman81 <afshan.aman@gmail.com>
2017-07-19 19:25:15 +0400

    added spinner to the weather page

--- localweather/script.js
+++ localweather/script.js
@@ -17,11 +17,13 @@ $(function(){
         var units       = "&units=metric"
         var appid_param = "&APPID=f09e057a05dc58c17d7baca4f36ab8d5";
         var weatherAPICall = url + loc_param + units + appid_param;
-        console.log(weatherAPICall)
+        //console.log(weatherAPICall)
 
         // get weather
         $.getJSON(weatherAPICall).done(function(data) {
-            console.log(data);
+            $('#spinner').hide();
+
+            //console.log(data);
             desc = data.weather[0].main;
             humidity = data.main.humidity;
             temp_c = Math.round(data.main.temp);
