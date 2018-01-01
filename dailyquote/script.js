8385211ce2d02201daf9ec4b6107b45380ed1c63
afshanaman81 <afshan.aman@gmail.com>
2018-01-01 21:44:51 +0400

    changed the API for daily quotes

--- dailyquote/script.js
+++ dailyquote/script.js
@@ -1,29 +1,32 @@
 $(document).ready(function(){
-	getQuote();
-	$("#new-quote").on("click", getQuote);
+	//getQuoteFromAndruxnet()
 
-	$('#project-details').on('click', function(){
-		// read XML file
-		$.ajax({
-			type: "GET" ,
-			url: "../portfolio/helpers/project-descriptions.xml" ,
-			dataType: "xml" ,
-			success: function(xml) {
-				$(xml).find('project').each(function() {
-					let project = {}
+	getQuoteFromQOD()
 
-					project.name = $(this).find('name').text()
-					project.desc = $(this).find('description').text()
-					console.log(project)
-				})
-			}
-		});
-
-		// Populate a card
-	})
+	// Register Event Listeners
+	$("#new-quote").on("click", getQuoteFromQOD);
+	// Not in use (but it reads an XML file and populates HTML elements)
+	$('#project-details').on('click', getProjectDescription)
 });
 
-function getQuote() {
+function getQuoteFromQOD(){
+	const httpsPreamble = "https://cors-anywhere.herokuapp.com/"
+	$.ajax( {
+		url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+		success: function(data) {
+			var post = data.shift(); // The data is an array of posts. Grab the first one.
+			var content = post.content
+			// remove the <p> and </p>
+			var quote = content.substr(3, content.length -8)
+			var result = {quote: quote, author: post.title}
+
+			updateUI(result)
+		},
+		cache: false
+	});
+
+}
+function getQuoteFromAndruxnet() {
 	$.ajax({
 		headers: {
 			"X-Mashape-Key": "UOfZLUoKknmshU12Rno8tdipdLybp1sZ78yjsn8aW1MogaaK1U",
@@ -31,33 +34,44 @@ function getQuote() {
 			"Content-Type": "application/x-www-form-urlencoded"
 		},
 		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
-		success: function(r) {
-			$("#quote-txt").animate({
-					opacity: 0
-				}, 500,
-				function() {
-					$(this).animate({
-						opacity: 1
-					}, 500);
+		success: function(result) {
+			updateUI(result)
+		}
+	});
+}
 
-					// todo: user r.quote instead, when the certificate is aquired
-					var quote = "When you realize you want to spend the rest of your life with somebody, " +
-						"you want the rest of your life to start as soon as possible."
-					var innerHTML = "<i class='fa fa-quote-left'></i>" + quote + "<i class='fa fa-quote-right'></i>";
-					$('#quote-txt').html(innerHTML);
-				});
+function updateUI(result){
+	// static content (if needed)
+	var quote = "When you realize you want to spend the rest of your life with somebody, " +
+		"you want the rest of your life to start as soon as possible."
+	var author = "When Harry Met Sally, 1989"
 
-			$("#quote-author").animate({
-					opacity: 0
-				}, 500,
-				function() {
-					$(this).animate({
-						opacity: 1
-					}, 500);
-					var author = "When Harry Met Sally, 1989"
-					$('#author').html("- " + author);
-				});
+	$("#quote-txt").animate({opacity: 0}, 500, function() {
+		$(this).animate({opacity: 1}, 500);
+		var innerHTML = "<i class='fa fa-quote-left'></i> " + result.quote + "<i class='fa fa-quote-right'></i>";
+		$('#quote-txt').html(innerHTML);
+	});
 
+	$("#quote-author").animate({opacity: 0}, 500, function() {
+		$(this).animate({opacity: 1}, 500);
+		$('#author').html("- " + result.author);
+	});
+}
+
+function getProjectDescription(){
+	// read XML file
+	$.ajax({
+		type: "GET" ,
+		url: "../portfolio/helpers/project-descriptions.xml" ,
+		dataType: "xml" ,
+		success: function(xml) {
+			$(xml).find('project').each(function() {
+				let project = {}
+
+				project.name = $(this).find('name').text()
+				project.desc = $(this).find('description').text()
+				console.log(project)
+			})
 		}
 	});
 }
